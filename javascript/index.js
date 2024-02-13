bdcaefd5d0108bc5d9289587e735cdfcaf8cea0f
Constanza Mora <constanza.morasan@gmail.com>
2024-02-13 18:54:08 -0400

    Made Selection box work.

--- javascript/index.js
+++ javascript/index.js
@@ -19,3 +19,23 @@ UpdateInitialTimeAndSetInterval("#halifax", "America/Halifax");
 UpdateInitialTimeAndSetInterval("#los-angeles", "America/Los_Angeles");
 UpdateInitialTimeAndSetInterval("#dortmund", "Europe/Berlin");
 UpdateInitialTimeAndSetInterval("#mexico-city", "America/Mexico_City");
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities-container");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2 class="destination">${cityTimeZone}</h2>
+            <div class="date">${cityTime.format("ddd Do MMM, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("HH:mm")}<small>${cityTime.format(
+    ":ss A"
+  )}<small></div>
+          </div>
+          `;
+}
