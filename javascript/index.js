091a171eca16bd46563b29a4b4c1a688a384193c
Constanza Mora <constanza.morasan@gmail.com>
2024-02-13 20:28:20 -0400

    Added the name of the cities selected in javascript, besides the interval was fixed. In addition it was added more cities to the option bar

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,21 @@
 function UpdateCityTime(cityID, timezone) {
   let cityElement = document.querySelector(cityID);
-  let cityDateElement = cityElement.querySelector(".date");
-  let cityTimeElement = cityElement.querySelector(".time");
-  let cityTime = moment().tz(timezone);
 
-  cityDateElement.innerHTML = cityTime.format("ddd Do MMM, YYYY");
-  cityTimeElement.innerHTML = `${cityTime.format(
-    "HH:mm[<small>]:ss A[<small>]"
-  )}`;
+  if (cityElement !== null) {
+    let cityDateElement = cityElement.querySelector(".date");
+    let cityTimeElement = cityElement.querySelector(".time");
+    let cityTime = moment().tz(timezone);
+
+    cityDateElement.innerHTML = cityTime.format("ddd Do MMM, YYYY");
+    cityTimeElement.innerHTML = `${cityTime.format(
+      "HH:mm[<small>]:ss A[<small>]"
+    )}`;
+  }
 }
 
 function UpdateInitialTimeAndSetInterval(cityID, timezone) {
   UpdateCityTime(cityID, timezone);
-  setInterval(UpdateCityTime, 1000, cityID, timezone);
+  return setInterval(UpdateCityTime, 1000, cityID, timezone);
 }
 
 UpdateInitialTimeAndSetInterval("#halifax", "America/Halifax");
@@ -23,14 +26,19 @@ UpdateInitialTimeAndSetInterval("#mexico-city", "America/Mexico_City");
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
 
+let selectedCityIntervalID;
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName = event.target[event.target.selectedIndex].label; // .replace("_", " ").split("/")[1];
+  if (cityTimeZone.length === 0) return;
+
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities-container");
   citiesElement.innerHTML = `
-  <div class="city">
+  <div class="city" id="selected-city">
           <div>
-            <h2 class="destination">${cityTimeZone}</h2>
+            <h2 class="destination">${cityName}</h2>
             <div class="date">${cityTime.format("ddd Do MMM, YYYY")}</div>
           </div>
           <div class="time">${cityTime.format("HH:mm")}<small>${cityTime.format(
@@ -38,4 +46,9 @@ function updateCity(event) {
   )}<small></div>
           </div>
           `;
+  clearInterval(selectedCityIntervalID);
+  selectedCityIntervalID = UpdateInitialTimeAndSetInterval(
+    "#selected-city",
+    cityTimeZone
+  );
 }
